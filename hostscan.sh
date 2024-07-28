71cc1f2e79376edf2e97b6fe96b194720ae229d1
Cristopher Rissardi <93612872+cristopherrissardi@users.noreply.github.com>
2024-07-28 01:55:26 -0300

    Atualização Hostscan

--- hostscan.sh
+++ hostscan.sh
@@ -1,48 +1,43 @@
 #!/bin/bash
 
+
 if [ "$1" == "" ]
+
 then 
-	echo ""
-	echo ""
-	echo ""
-	echo "██╗  ██╗ ██████╗ ███████╗████████╗    ███████╗ ██████╗ █████╗ ███╗   ██╗
-██║  ██║██╔═══██╗██╔════╝╚══██╔══╝    ██╔════╝██╔════╝██╔══██╗████╗  ██║
-███████║██║   ██║███████╗   ██║       ███████╗██║     ███████║██╔██╗ ██║
-██╔══██║██║   ██║╚════██║   ██║       ╚════██║██║     ██╔══██║██║╚██╗██║
-██║  ██║╚██████╔╝███████║   ██║       ███████║╚██████╗██║  ██║██║ ╚████║
-╚═╝  ╚═╝ ╚═════╝ ╚══════╝   ╚═╝       ╚══════╝ ╚═════╝╚═╝  ╚═╝╚═╝  ╚═══╝
-                                                                        "
-	echo "		 ---------------------------------------"
-	echo "		 --  Created By Christopher Rissardi  --"
-	echo "		 ---------------------------------------"
-	echo ""
-	echo "MODO DE USO: $0 IP"
-	echo "Exemplo: $0 192.168.0"
-	echo "OBS: Essa ferramenta foi criada apenas para scanear hosts ativos na rede!"
+
+	echo "
+
+			
+		░█░█░█▀█░█▀▀░▀█▀░█▀▀░█▀▀░█▀█░█▀█
+		░█▀█░█░█░▀▀█░░█░░▀▀█░█░░░█▀█░█░█   https://github.com/cristopherrissardi/Hostscan
+		░▀░▀░▀▀▀░▀▀▀░░▀░░▀▀▀░▀▀▀░▀░▀░▀░▀  ------------------------------------------------
+
+
+Use: $0 + IP (Sem o último número)
+Exemplo: $0 10.0.0.
+O último dígito será varrido pela ferramenta"
+
 else
 
+	echo "
 
-        echo ""
-        echo ""
-        echo ""
-        echo "██╗  ██╗ ██████╗ ███████╗████████╗    ███████╗ ██████╗ █████╗ ███╗   ██╗
-██║  ██║██╔═══██╗██╔════╝╚══██╔══╝    ██╔════╝██╔════╝██╔══██╗████╗  ██║
-███████║██║   ██║███████╗   ██║       ███████╗██║     ███████║██╔██╗ ██║
-██╔══██║██║   ██║╚════██║   ██║       ╚════██║██║     ██╔══██║██║╚██╗██║
-██║  ██║╚██████╔╝███████║   ██║       ███████║╚██████╗██║  ██║██║ ╚████║
-╚═╝  ╚═╝ ╚═════╝ ╚══════╝   ╚═╝       ╚══════╝ ╚═════╝╚═╝  ╚═╝╚═╝  ╚═══╝
-                                                                        "
-        echo "		---------------------------------------"
-        echo "		--  Created By Christopher Rissardi  --"
-        echo "		---------------------------------------"
-	echo ""
-	echo "OBS: A FERRAMENTA SO RETORNARA HOSTS ENCONTRADOS!"
-	echo "O SCAN PODE DEMORAR DEVIDO A QUANTIDADE DE HOSTS PRESENTE EM SUA REDE"
-        echo ""
+			
+		░█░█░█▀█░█▀▀░▀█▀░█▀▀░█▀▀░█▀█░█▀█
+		░█▀█░█░█░▀▀█░░█░░▀▀█░█░░░█▀█░█░█   https://github.com/cristopherrissardi/Hostscan
+		░▀░▀░▀▀▀░▀▀▀░░▀░░▀▀▀░▀▀▀░▀░▀░▀░▀  ------------------------------------------------
 
+
+- O script retorna somente hosts ativos. 
+- Pode demorar bastante a varredura.
+- Outros detalhes deixarei no repositório!
+
+
+# # # # # # # # # # # # # # # # # # # # # # 
+"
 
 for host in {1..254};
 do
-ping -i 0.1 -c 1 $1.$host | grep "64 bytes" | grep -v "100%" | grep -v "Destination Host Unreachable" | cut -d " " -f 4 | sed 's/.$/ <<-------------- HOST ENCONTRADO/';	
+ping $1$host -i 0.1 -c 1 | grep "64 bytes" | grep -v "100%" | grep -v "Destination Host Unreachable" | cut -d " " -f 4 | sed 's/.$/ <<-------------- Host Encontrado!/';	
 done
+echo ""
 fi
